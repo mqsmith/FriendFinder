bb78e2248808abdc1f7f292e3385e8d4d49728c6
Mike Smith <smith.mike@mac.com>
2019-08-28 09:37:58 -0400

    Added some data to the friends.js file and exported it.

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Benjamin Browder",
+		photo: "https://www.fillmurray.com/200/300",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Vinny Testaverde",
+		photo: "https://www.fillmurray.com/g/200/300",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Brian Eastman",
+		photo: "https://www.fillmurray.com/460/300",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
