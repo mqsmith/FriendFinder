be9cd3402547c520c59888bae61423829fac4de2
Mike Smith <smith.mike@mac.com>
2019-08-28 09:42:04 -0400

    parsed out the different js files and requried them in the main server.js.

--- server.js
+++ server.js
@@ -7,17 +7,12 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+require("app/routing/apiRoutes")(app);
+require("app/routing/htmlRoutes")(app);
+
+
 app.listen(PORT, function(){
     console.log("App listening on http://localhost:" + PORT);
     // console.log("Reservations: " + newReservation.customerName);
 });
 
-//Routes
-
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
